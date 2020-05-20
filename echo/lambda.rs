7be96fba7be7a34edce4b505f139a2d095223b83
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2020-05-20 17:33:08 +0200

    Let the Rust Lambda log something

--- echo/lambda.rs
+++ echo/lambda.rs
@@ -1,11 +1,19 @@
-use lambda::lambda;
+use lambda::handler_fn;
+use log::info;
 use serde_json::Value;
+use std::env;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-#[lambda]
 #[tokio::main]
-async fn main(event: Value) -> Result<Value, Error> {
-    // Return the input event as output
+async fn main() -> Result<(), Error> {
+    simple_logger::init_with_level(log::Level::Debug)?;
+    lambda::run(handler_fn(handler)).await?;
+    Ok(())
+}
+
+// Return the input event as output
+async fn handler(event: Value) -> Result<Value, Error> {
+    info!("Hello from {}!", env::var("AWS_LAMBDA_FUNCTION_NAME")?);
     Ok(event)
 }
